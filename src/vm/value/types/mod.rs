2617e32d6d7316692e8f7cb5ffc814ae5d7a0f78
LoganDark <logandarklock@gmail.com>
2021-11-07 17:05:20 -0800

    LuauType -> DynValue

--- src/vm/value/types/mod.rs
+++ src/vm/value/types/mod.rs
@@ -14,19 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use crate::vm::{ThreadUserdata, Value};
-
 pub mod function;
-
-pub enum LuauType<'borrow, 'thread: 'borrow, UD: ThreadUserdata> {
-	Nil,
-	Boolean(bool),
-	LightUserdata(*mut std::ffi::c_void),
-	Number(f64),
-	Vector([f32; 3]),
-	String(Value<'borrow, 'thread, UD>),
-	Table(Value<'borrow, 'thread, UD>),
-	Function(Value<'borrow, 'thread, UD>),
-	Userdata(Value<'borrow, 'thread, UD>),
-	Thread(Value<'borrow, 'thread, UD>)
-}
